a0e69dc3f5a7ff52702c2384b7948e96fbbf42ec
Carlosjosetri <37202994+Carlosjosetri@users.noreply.github.com>
2020-05-25 23:28:07 -0600

    fix error de deo

--- src/main/java/com/uca/capas/JPAconfiguration.java
+++ src/main/java/com/uca/capas/JPAconfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-public class JPAConfiguration {
+public class JPAconfiguration {
 	
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
